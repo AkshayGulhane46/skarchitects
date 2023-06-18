005fa01876054801f41f53c999e7e94c5fc3bc5d
Akshay Gulhane <argulhane10246@gmail.com>
2023-06-18 23:27:36 +0530

    commit1806231

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router , Navigate , Route } from 'react-router-dom'
-
+import { BrowserRouter as Router , Navigate , Route, Routes } from 'react-router-dom'
+import Home from './components/Pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar/>
+        <Routes>
+          <Route path='/' Component={Home}/>
+        </Routes>
       </Router>
 
     </div>
