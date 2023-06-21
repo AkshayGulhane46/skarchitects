9aff11d8d8efdf0dfdcecd537a4603338c4b0e64
Akshay Gulhane <argulhane10246@gmail.com>
2023-06-21 23:13:15 +0530

    Commit2106231

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -3,7 +3,11 @@ import { Link } from 'react-router-dom'
 
 function CardItem() {
   return (
-    <div>CardItem</div>
+    <li className='cards_item'>
+        <Link className='cards__item__link'>
+            
+        </Link>
+    </li>
   )
 }
 
