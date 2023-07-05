18cf628ad8843c03548a186a9937dd86edcab333
Akshay Gulhane <argulhane10246@gmail.com>
2023-07-05 12:53:49 +0530

    added images

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,75 +1,14 @@
-import { useState } from 'react'
-
-
-import './wsp-gallery.css'
-
-const WSPGallery = ({galleryImages}) => {
-
-  const [slideNumber, setSlideNumber] = useState(0)
-  const [openModal, setOpenModal] = useState(false)
-
-  const handleOpenModal = (index) => {
-    setSlideNumber(index)
-    setOpenModal(true)
-  }
-
-  // Close Modal
-  const handleCloseModal = () => {
-    setOpenModal(false)
-  }
-
-  // Previous Image
-  const prevSlide = () => {
-    slideNumber === 0 
-    ? setSlideNumber( galleryImages.length -1 ) 
-    : setSlideNumber( slideNumber - 1 )
-  }
-
-  // Next Image  
-  const nextSlide = () => {
-    slideNumber + 1 === galleryImages.length 
-    ? setSlideNumber(0) 
-    : setSlideNumber(slideNumber + 1)
-  }
-
-  return (
-    <div>
-
-      {openModal && 
-        <div className='sliderWrap'>
-          <FontAwesomeIcon icon={faCircleXmark} className='btnClose' onClick={handleCloseModal}  />
-          <FontAwesomeIcon icon={faCircleChevronLeft} className='btnPrev' onClick={prevSlide} />
-          <FontAwesomeIcon icon={faCircleChevronRight} className='btnNext' onClick={nextSlide} />
-          <div className='fullScreenImage'>
-            <img src={galleryImages[slideNumber].img} alt='' />
-          </div>
-        </div>
-      }
-
-      {/* <br />
-      Current slide number:  {slideNumber}
-      <br />
-      Total Slides: {galleryImages.length}
-      <br /><br /> */}
-
-      <div className='galleryWrap'>
-        {
-          galleryImages && galleryImages.map((slide, index) => {
-            return(
-              <div 
-                className='single' 
-                key={index}
-                onClick={ () => handleOpenModal(index) }
-              >
-                <img src={slide.img} alt='Image Template' />
-              </div>
-            )
-          })
-        }
-      </div>
-
-    </div>
-  )
-}
-
-export default WSPGallery
+export const images = [
+  {
+    original: 'https://burst.shopifycdn.com/photos/two-customized-cars-rolling-through-a-city-street.jpg?width=976&format=pjpg&exif=1&iptc=1',
+    thumbnail: 'https://burst.shopifycdn.com/photos/two-customized-cars-rolling-through-a-city-street.jpg?width=100&format=pjpg&exif=1&iptc=1',
+  },
+  {
+    original: 'https://burst.shopifycdn.com/photos/wooden-toothbrushes-lined-up-against-a-green-background.jpg?width=925&format=pjpg&exif=1&iptc=1',
+    thumbnail: 'https://burst.shopifycdn.com/photos/wooden-toothbrushes-lined-up-against-a-green-background.jpg?width=100&format=pjpg&exif=1&iptc=1',
+  },
+  {
+    original: 'https://burst.shopifycdn.com/photos/painter-working-in-studio.jpg?width=925&format=pjpg&exif=1&iptc=1',
+    thumbnail: 'https://burst.shopifycdn.com/photos/painter-working-in-studio.jpg?width=100&format=pjpg&exif=1&iptc=1',
+  },
+];
