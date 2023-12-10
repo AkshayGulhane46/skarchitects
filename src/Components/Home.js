6efda39c30160a2510f1a0d74f380bf9b4042c69
Akshay Gulhane <argulhane10246@gmail.com>
2023-12-10 15:53:56 +0530

    Commit1012231

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,18 +3,11 @@ import "../Styles/Home.scss";
 import { useParallax } from "react-scroll-parallax";
 
 const Home = () => {
-  const [showInfo, setShowInfo] = useState(false);
+  const [isBlurred, setBlurred] = useState(false);
 
-  const toggleProjectInfo = () => {
-    console.log("infor shows")
-    setShowInfo(!showInfo);
+  const toggleBlur = () => {
+    setBlurred(!isBlurred);
 }
-
-const navigateToProjectDetails = () => {
-  // You can replace '/project-details' with the actual path to your project details page
-  
-};
-
 
   return (
     <>
@@ -33,60 +26,30 @@ const navigateToProjectDetails = () => {
       </div>
       <div className='middle'>
         <div className='projects'>
-        <div className={`project-1 ${showInfo ? 'show-info' : ''}`} onClick={toggleProjectInfo}>
-      <div className='project-name'>
-        <p> The Farmhouse </p>
-        <p className="project-info">Some information about the project</p>
-        {showInfo && (
-          <button className="more-info-button" onClick={navigateToProjectDetails}>
-            More Info
-          </button>
-        )}
-      </div>
-      <div className='img-container'>
-        <img src='farmhouse3.jpg' alt='Farmhouse Image' />
-      </div>
-    </div>
-    <div className={`project-1 ${showInfo ? 'show-info' : ''}`} onClick={toggleProjectInfo}>
-      <div className='project-name'>
-        <p> The Farmhouse </p>
-        <p className="project-info">Some information about the project</p>
-        {showInfo && (
-          <button className="more-info-button" onClick={navigateToProjectDetails}>
-            More Info
-          </button>
-        )}
-      </div>
-      <div className='img-container'>
-        <img src='building3.jpg'></img>      
-        </div>
-    </div>
-
-           <div className='project-1'>
-              <div className='project-name'>
-                <p> The Farmhouse </p>
-              </div> 
-              <div className='img-container'>
-                <img src='building4.png'></img>
-              </div>
-          </div>
-           <div className='project-1'>
-              <div className='project-name'>
-                <p> The Farmhouse </p>
-              </div> 
-              <div className='img-container'>
-                <img src='building5.png'></img>
-              </div>
-          </div>
-        </div>
         <div className='project-1'>
-              <div className='project-name'>
-                <p> The Farmhouse </p>
-              </div> 
-              <div className='img-container'>
-                <img src='building21.png'></img>
-              </div>
+      <div
+        className={`img-container ${isBlurred ? 'blur' : ''}`}
+        onClick={toggleBlur}
+      >
+        <img src='building1.png' alt='Building' />
+        <div className='overlay-text'>
+          <div className='project-name'>
+            <p>The Farmhouse</p>
           </div>
+          <div className='project-details'>
+            <p> This is the farmhouse </p>
+          </div>
+          <div className='project-button'>
+            <button>Learn More</button>
+          </div>
+        </div>
+      </div>
+
+
+      </div>
+
+        </div>
+        
       </div>
       
     </>
