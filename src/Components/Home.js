06b85facdc7e27eeabda2dac3e3319769a6293f4
Akshay Gulhane <argulhane10246@gmail.com>
2023-12-18 23:13:44 +0530

    COmmit1812231

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import "../Styles/Home.scss";
 import { useParallax } from "react-scroll-parallax";
 
@@ -7,54 +7,42 @@ const Home = () => {
 
   const toggleBlur = () => {
     setBlurred(!isBlurred);
-}
+  };
 
   return (
     <>
       <div className='top'>
-          <h1>we are</h1>
-          <p>SK ARCHITECTS</p>
-          <div className='img-container'>
-              <img src='building1.png'></img>
-          </div>
-  
+        <h1>we are</h1>
+        <p>SK ARCHITECTS</p>
+        <div className='img-container'>
+          <img src="" alt='Building'></img>
+        </div>
       </div>
       <div className='our-work'>
-        <div className='heading'>
-
-        </div>
+        <div className='heading'></div>
       </div>
       <div className='middle'>
         <div className='projects'>
-        <div className='project-1'>
-      <div
-        className={`img-container ${isBlurred ? 'blur' : ''}`}
-        onClick={toggleBlur}
-      >
-        <img src='building1.png' alt='Building' />
-        <div className='overlay-text'>
-          <div className='project-name'>
-            <p>The Farmhouse</p>
-          </div>
-          <div className='project-details'>
-            <p> This is the farmhouse </p>
-          </div>
-          <div className='project-button'>
-            <button>Learn More</button>
+          <div className='project-1'>
+            <div className="img-container">
+              <img src='' alt='Building'></img>
+              <div className='overlay-text'>
+                <div className='project-name'>
+                  <p>The Farmhouse</p>
+                </div>
+                <div className='project-details'>
+                  <p>This is the farmhouse</p>
+                </div>
+                <div className='project-button'>
+                  <button>Learn More</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-
-
-      </div>
-
-        </div>
-        
-      </div>
-      
     </>
   );
 };
 
 export default Home;
-
